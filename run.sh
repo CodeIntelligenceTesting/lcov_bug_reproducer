246fece1120f94701d55576ed5e2784a8e34cc66
Robert Czechowski <robert@code-intelligence.com>
2024-12-11 13:45:19 +0100

    fix run.sh for shrinkray

--- run.sh
+++ run.sh
@@ -2,7 +2,21 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-rm -rf genlog
+if [[ "$PWD" != ~/test/ltest ]]
+then
+#  cp ~/test/ltest/lzo_conf.h .
+  cp ~/test/ltest/lzoconf.h .
+  cp ~/test/ltest/lzodefs.h .
+  cp ~/test/ltest/lzo_dll.ch .
+  cp ~/test/ltest/lzo_func.h .
+  cp ~/test/ltest/lzo_init.c .
+  cp ~/test/ltest/lzo_ptr.h .
+  cp ~/test/ltest/lzo_supp.h .
+  cp ~/test/ltest/lzo_util.c .
+  cp ~/test/ltest/main.c .
+fi
+
+rm -f genlog
 
 clang -o main -fcoverage-mapping -fprofile-instr-generate main.c lzo_init.c lzo_util.c  -I .
 
@@ -14,4 +28,5 @@ llvm-cov export -format=lcov -instr-profile=main.profdata main > main.lcov
 
 genhtml main.lcov 2> genlog || true
 
-grep -q 'genhtml: ERROR: (range) /home/robert/test/ltest/lzo_supp.h contains only' genlog
+grep -q 'genhtml: ERROR: (range)' genlog
+grep -q 'lzo_supp.h contains only' genlog
